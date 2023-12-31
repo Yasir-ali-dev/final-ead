3e78a3fca1bef4724abdff7aa2c4daa624de9bf6
Yasir Ali <yasirali.bscssef20@iba-suk.edu.pk>
2023-12-31 19:47:30 +0500

    update

---
+++ src/context/FinalExamContext.js
@@ -0,0 +1,26 @@
+import React, { createContext, useContext, useState } from "react";
+
+export const MyExamContext = createContext();
+
+export const MyExamInformationProvider = ({ children }) => {
+  const [state, setState] = useState("jijj");
+  return (
+    <MyExamContext.Provider
+      value={{
+        state,
+        setState,
+      }}
+    >
+      {children}
+    </MyExamContext.Provider>
+  );
+};
+
+export const useExamContext = () => {
+  const context = useContext(MyExamContext);
+  console.log(context);
+  if (!context) {
+    throw new Error("useMyContext must be used within a MyProvider");
+  }
+  return context;
+};
